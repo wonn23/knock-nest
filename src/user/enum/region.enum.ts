d2464eaa8cb494fab453557ba7eda03388f42833
heochangwon <wonn22@gmail.com>
2024-06-18 06:49:49 +0900

    feat: region enum 작성

---
+++ src/user/enum/region.enum.ts
@@ -0,0 +1,17 @@
+enum Region {
+  SEOUL = '서울',
+  BUSAN = '부산',
+  DAEGU = '대구',
+  INCHEON = '인천',
+  GWANGJU = '광주',
+  ULSAN = '울산',
+  SEJONG = '세종',
+  GYEONGGI = '경기도',
+  GANGWON = '강원도',
+  CHUNGCHEONG = '충청도',
+  JEOLLA = '전라도',
+  GYEONGSANG = '경상도',
+  JEJU = '제주',
+}
+
+export default Region;
