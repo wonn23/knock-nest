a31746418719a84b0245f4877f1dd8d528d0c76d
heochangwon <wonn22@gmail.com>
2024-06-18 11:09:55 +0900

    feat: 유저커스텀데코레이터 구현

---
+++ src/common/decorator/current-user.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
+
+// 요청안에 있는 user 값을 가지고 온다.
+export const CurrentUser = createParamDecorator(
+  (data, ctx: ExecutionContext): User => {
+    const req = ctx.switchToHttp().getRequest();
+    return req.user;
+  },
+);
